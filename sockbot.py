ba542f60f26587c812568d9dd5a05c6a21f06d91
Simon AM <simon@agius-muscat.net>
2015-10-23 17:08:16 +0200

    Switched to a comment parsing setup

--- sockbot.py
+++ sockbot.py
@@ -11,25 +11,22 @@ def main():
     o = OAuth2Util.OAuth2Util(r, server_mode=True)
     o.refresh(force=True)
     while True:
-        print('[+] Sockbot is alive!')
+        print('[+] Sockbot is starting a cycle')
         try:
             subreddit = r.get_subreddit('elitedangerous')
         except Exception:
             print('[-] ', Exception)
             exit(0)
-        print('[+] Sockbot is retrieving submissions')
-        submissions = subreddit.get_hot(limit=10)  # Set limit in config
-        for submission in submissions:
-            print('[+] Sockbot found thread: ', submission)
-            comments = praw.helpers.flatten_tree(submission.comments)
-            print('[+] Sockbot is going to check the comments for the word sock')
-            for comment in comments:
-                print(comment.body)
-                if 'sock' in comment.body:  # TODO: check for uppercase SOCK
-                    print('[+] Sockbot found a sock!!!!!')
-                    sleep(10)
-                sleep(1)
-            sleep(5)
+        print('[+] Sockbot is retrieving comments')
+        all_comments = r.get_comments('elitedangerous') # add other elite dangerous subreddits here
+        for comment in all_comments:
+            print(comment.body)
+            if 'sock' in comment.body:
+                print('[+] Sockbot found a sock!!!!')
+                # add send a message function here
+                sleep(5)
+            sleep(0.1)
+        print('[+] Sockbot has finished a cycle')
         sleep(5)
 
 if __name__ == '__main__':
