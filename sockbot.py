da38e8d373d57d8e879ca0a7eafbf480071f34a0
Simon AM <simon@agius-muscat.net>
2015-10-23 17:54:23 +0200

    Added verbose mode option

--- sockbot.py
+++ sockbot.py
@@ -7,9 +7,17 @@ from time import sleep
 version = '0.1'
 user_agent = 'windows:sockbot:v{} (by /u/Always_SFW)'.format(version)
 testing_mode = True  # switch to test DB
+verbose_mode = False
 table = 'socks'
 if testing_mode:
     table = 'test'
+
+def pause(time=5):
+    time_left = time
+    while time_left >= 0:
+        print('[+] Sockbot will continue in: {}  '.format(time_left), end='\r')
+        time_left -= 1
+        sleep(1)
 
 def get_old_socks(cursor, table_name=table):
     old_socks = set()
@@ -34,29 +42,26 @@ def main():
         print('[+] Sockbot is retrieving comments')
         all_comments = r.get_comments('elitedangerous') # add other elite dangerous subreddits here
         for comment in all_comments:
-            try:
-                print(comment.body)
-            except Exception:
-                print('[-] Could not print comment!')
-                print(Exception)
-            else:
-                if 'CMDR' in comment.body:
-                    print('[+] Sockbot found a sock!!!!')
-                    # check if the sock is already in the database
-                    if comment.id in get_old_socks(dbcur, table):
-                        print('[+] Sockbot already has a record of this entry')
-                    else:
-                        # add the comment to the database
-                        dbcur.execute('INSERT INTO {} VALUES (NULL, ?, CURRENT_TIMESTAMP)'.format(table), (comment.id,))
-                        dbcon.commit()
-                        # add send a message function here
-                        sleep(2)
+            if verbose_mode:
+                try:
+                    print(comment.body)
+                except Exception:
+                    print('[-] Could not print comment!')
+                    print(Exception)
+            if 'CMDR' in comment.body:
+                print('[+] Sockbot found a sock!!!!')
+                # check if the sock is already in the database
+                if comment.id in get_old_socks(dbcur, table):
+                    print('[+] Sockbot already has a record of this entry')
+                else:
+                    # add the comment to the database
+                    dbcur.execute('INSERT INTO {} VALUES (NULL, ?, CURRENT_TIMESTAMP)'.format(table), (comment.id,))
+                    dbcon.commit()
+                    # add send a message function here
+                    pause(2)
+        print('[!] Amount of socks sent: ', len(get_old_socks(dbcur, table)))
         print('[+] Sockbot has finished a cycle')
-        print('Contents of db:')
-        for row in dbcur.execute('SELECT * FROM {} ORDER BY id'.format(table)):
-            print(row)
-        print('[+] Sockbot will start another cycle in 5 seconds')  # replace with pause function
-        sleep(5)
+        pause(5)
 
 if __name__ == '__main__':
     main()
