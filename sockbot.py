52b61d429cd88cd4bd97adbc5336cb0f2ce0b5d1
Simon AM <simon@agius-muscat.net>
2015-10-23 16:42:06 +0200

    Switched to oauth.ini for easier connection

--- sockbot.py
+++ sockbot.py
@@ -1,10 +1,18 @@
-import config
 import praw
+import OAuth2Util
+
+from time import sleep
 
 version = '0.1'
+user_agent = 'windows:sockbot:v{} (by /u/Always_SFW)'.format(version)
 
 def main():
-    r = praw.Reddit(user_agent=config.user_agent)
+    r = praw.Reddit(user_agent=user_agent)
+    o = OAuth2Util.OAuth2Util(r, server_mode=True)
+    while True:
+        o.refresh()
+        print(r.get_me().comment_karma)
+        sleep(3600)
 
 if __name__ == '__main__':
     main()
