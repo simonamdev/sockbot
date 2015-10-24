ee5d194382b09cbe9006431b5c17e46a53287705
Simon AM <simon@agius-muscat.net>
2015-10-24 16:42:47 +0200

    Attempting to catch HTTP exception

--- sockbot.py
+++ sockbot.py
@@ -45,31 +45,34 @@ def main():
         print('[+] Sockbot cycle: ', cycle)
         try:
             all_comments = r.get_comments('elitedangerous') # add other elite dangerous subreddits here
-        except Exception:
+        except:
             print('[-] ', Exception)
         else:
             print('[+] Sockbot is looking through the comments for: ', words)
             instances = 0  # of the word present in the comments. Also includes those already in DB
-            for comment in all_comments:
-                for word in words:
-                    if word in comment.body and not word in avoid_words and not 'I am a bot!' in comment.body:  # need to make a list of actual non sock references
-                        # if sock is not in the database, put it in and contact the user
-                        instances += 1
-                        if not comment.id in get_old_socks(dbcur, table):
-                            # add the comment to the database, then send the message
-                            print('[!] Sockbot found a sock! Placing ID: {} in the database'.format(comment.id))
-                            dbcur.execute('INSERT INTO {} VALUES (NULL, ?, CURRENT_TIMESTAMP)'.format(table), (comment.id,))
-                            dbcon.commit()
-                            pk_id = dbcur.execute('SELECT max(id) FROM {}'.format(table)).fetchone()[0]
-                            message_string = 'Sock #{} was spotted at: {}.  If I broke somehow, contact /u/Always_SFW! or get /u/SpyTec13 to ban me!'.format(pk_id, comment.permalink)
-                            print('[!] Sending string:')
-                            print('\t', message_string)
-                            r.send_message(user, 'Sock #{} spotted!'.format(pk_id), message_string)
-                            reply_string = 'I see you mentioned socks in some way. I have notified tfaddy. I am a bot! You can find my source code here: https://github.com/Winter259/sockbot'
-                            print('[!] Replying with:')
-                            print('\t', reply_string)
-                            comment.reply(reply_string)
-                            pause(send_delay)
+            try:
+                for comment in all_comments:
+                    for word in words:
+                        if word in comment.body and word not in avoid_words and 'I am a bot!' not in comment.body:  # need to make a list of actual non sock references
+                            # if sock is not in the database, put it in and contact the user
+                            instances += 1
+                            if comment.id not in get_old_socks(dbcur, table):
+                                # add the comment to the database, then send the message
+                                print('[!] Sockbot found a sock! Placing ID: {} in the database'.format(comment.id))
+                                dbcur.execute('INSERT INTO {} VALUES (NULL, ?, CURRENT_TIMESTAMP)'.format(table), (comment.id,))
+                                dbcon.commit()
+                                pk_id = dbcur.execute('SELECT max(id) FROM {}'.format(table)).fetchone()[0]
+                                message_string = 'Sock #{} was spotted at: {}.  If I broke somehow, contact /u/Always_SFW! or get /u/SpyTec13 to ban me!'.format(pk_id, comment.permalink)
+                                print('[!] Sending string:')
+                                print('\t', message_string)
+                                r.send_message(user, 'Sock #{} spotted!'.format(pk_id), message_string)
+                                reply_string = 'I see you mentioned socks in some way. I have notified tfaddy. I am a bot! You can find my source code here: https://github.com/Winter259/sockbot'
+                                print('[!] Replying with:')
+                                print('\t', reply_string)
+                                comment.reply(reply_string)
+                                pause(send_delay)
+            except:
+                print('[-] Encountered exception: {} when trying to search the comments'.format(Exception))
             cycle += 1
             print('[+] Current amount of socks in DB: {}, Instances found this cycle: {}'.format(len(get_old_socks(dbcur, table)), instances))
             pause(cycle_delay)
