f2de6beb85e972a4d2153e7547890fa945a9c63c
Simon AM <simon@agius-muscat.net>
2015-12-11 18:09:04 +0100

    Simplified code structure, added comment body to message

--- sockbot.py
+++ sockbot.py
@@ -13,13 +13,11 @@ no employee of Frontier Developments was involved in the making of it.
 
 version = '0.7.4'
 user_agent = 'raspberrypi:sockb0t259:v{} (by /u/Always_SFW)'.format(version)
-testing_mode = False  # switch to test DB and criteria
-words_to_look_for = ['sock', 'SOCK', 'Sock']
-words_to_avoid = ['socket', 'SOCKET', 'Socket']
 table = 'socks'
-user = 'tfaddy'
 send_delay = 5
 cycle_delay = 1
+words_to_avoid = ['Socket', 'Sockpuppet']
+user = 'tfaddy'
 subreddits = [
     'elitedangerous',
     'elitetraders',
@@ -62,63 +60,72 @@ def get_comment_id_list(db, table_name):
 
 
 def main():
+    thread_commenters = dict()
     cycle = 1
     startup_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     print('[!] Sockbot version {} starting at: {}'.format(version, startup_time))
-    database = Pysqlite('socksdb', 'socks.db')
+    database = Pysqlite('socksdb', 'socks.db')  # initialise the database
     r = praw.Reddit(user_agent=user_agent)
     o = OAuth2Util.OAuth2Util(r, server_mode=True)
     o.refresh(force=True)  # avoids having to refresh the token manually
     while True:
-        print('[+] Sockbot cycle: {}. Searching for: {}'.format(cycle, words_to_look_for))
+        print('[+] Sockbot cycle: {}'.format(cycle))
         cycle += 1
         socks_spotted = 0  # amount of socks currently present in the comments. Also includes those already in DB
         for subreddit in subreddits:
             try:
                 all_comments = r.get_comments(subreddit)
             except Exception as e:
-                print('[-] Exception occurred:', e)
+                print('[-] Exception occurred: {}'.format(e))
             else:
                 print('[+] Sockbot is checking /r/{}'.format(subreddit))
                 try:
                     for comment in all_comments:
-                        for word in words_to_look_for:
-                            if word in comment.body and 'tfaddy has been notified' not in comment.body:  # need to make a list of actual non sock references
-                                avoid_word_present = False  # check for words you are supposed to avoid, like 'Socket'
-                                for bad_word in words_to_avoid:
-                                    if bad_word in comment.body:
-                                        print('[-] Comment with ID: {} at link: {} has a bad word in it'.format(comment.id, comment.permalink))
-                                        avoid_word_present = True
-                                        break
-                                if avoid_word_present:
-                                    break  # avoid extra iterations
+                        print('[+] Checking comment with ID: {}    '.format(comment.id), end='\r')
+                        if 'sock' in comment.body.lower() and not comment.author.name == 'sockbot259':
+                            avoid_word_present = False  # check for words you are supposed to avoid, like 'Socket'
+                            for bad_word in words_to_avoid:
+                                if bad_word in comment.body:
+                                    print('[-] Comment with ID: {} at link: {} is invalid'.format(comment.id, comment.permalink))
+                                    avoid_word_present = True
+                                    break
+                            if avoid_word_present:
+                                break  # avoid extra iterations, go to the next comment
+                            else:
+                                # if sock is not in the database, put it in and contact the user
+                                socks_spotted += 1
+                                if comment.id not in get_comment_id_list(database, table):
+                                    # add the comment to the database, then send the message
+                                    print('[!] Sockbot found a sock! Placing ID: {} in the database'.format(comment.id))
+                                    database.insert_db_data(table, '(NULL, ?, CURRENT_TIMESTAMP)', (comment.id,))
+                                    pk_id = database.dbcur.execute('SELECT max(id) FROM {}'.format(table)).fetchone()[0]
+                                    message_string = '<h1>Sock #{} was spotted at: {}.</h1><br><hr><br>' \
+                                                     'Post contents:<br>' \
+                                                     '<p>{}</p>'.format(
+                                                        pk_id,
+                                                        comment.permalink,
+                                                        comment.body
+                                                     )
+                                    if not subreddit == 'EiteDagerous':
+                                        print('[!] Sending string: {}'.format(html2text(message_string)))
+                                        r.send_message(user, 'Sock #{} spotted!'.format(pk_id), html2text(message_string))  # user, title, contents
+                                    reply_string = '<h1>SOCK DETECTED</h1><br><br>' \
+                                                   'tfaddy has been notified.<br><hr><br>' \
+                                                   '<i>I am a bot, created and maintained by <a href ="https://www.reddit.com/user/Always_SFW">CMDR Purrcat</a><br>' \
+                                                   'Click <a href="https://www.reddit.com/r/EliteDangerous/comments/3sz817/learn_how_to_get_ripped_in_4_weeks/cx261wx">here</a> to find out why I exist.<br>' \
+                                                   'You can find my source code <a href="https://github.com/Winter259/sockbot">on github</a><br>' \
+                                                   'Socks detected so far: <b>{}</b><br>' \
+                                                   'Online since: <b>{}</b> (GMT)<br>' \
+                                                   'SOCKBOT IS HYPED FOR HORIZONS!! ARE YOU??<br>' \
+                                                   'Sockbot current version: <b>{}</b></i>'.format(pk_id, startup_time, version)
+                                    post_string = html2text(reply_string)
+                                    if subreddit == 'EiteDagerous':
+                                        post_string = 'ಠ_ಠ'
+                                    print('[!] Replying with: {}'.format(post_string))
+                                    comment.reply(post_string)
+                                    pause('Holding after sending message', send_delay)
                                 else:
-                                    # if sock is not in the database, put it in and contact the user
-                                    socks_spotted += 1
-                                    if comment.id not in get_comment_id_list(database, table):
-                                        # add the comment to the database, then send the message
-                                        print('[!] Sockbot found a sock! Placing ID: {} in the database'.format(comment.id))
-                                        database.insert_db_data(table, '(NULL, ?, CURRENT_TIMESTAMP)', (comment.id,))
-                                        pk_id = database.dbcur.execute('SELECT max(id) FROM {}'.format(table)).fetchone()[0]
-                                        message_string = 'Sock #{} was spotted at: {}. If I broke somehow, contact /u/Always_SFW! or get /u/SpyTec13 to ban me! I have been online since: {}'.format(pk_id, comment.permalink, startup_time)
-                                        if not subreddit == 'EiteDagerous':
-                                            print('[!] Sending string: {}'.format(message_string))
-                                            r.send_message(user, 'Sock #{} spotted!'.format(pk_id), message_string)
-                                        reply_string = '<h1>SOCK DETECTED</h1><br><br>' \
-                                                       'tfaddy has been notified.<br><hr><br>' \
-                                                       '<i>I am a bot, created and maintained by <a href ="https://www.reddit.com/user/Always_SFW">CMDR Purrcat</a><br>' \
-                                                       'Click <a href="https://www.reddit.com/r/EliteDangerous/comments/3sz817/learn_how_to_get_ripped_in_4_weeks/cx261wx">here</a> to find out why I exist.<br>' \
-                                                       'You can find my source code <a href="https://github.com/Winter259/sockbot">on github</a><br>' \
-                                                       'Socks detected so far: <b>{}</b><br>' \
-                                                       'Online since: <b>{}</b> (GMT)<br>' \
-                                                       'SOCKBOT IS HYPED FOR HORIZONS!! ARE YOU??<br>' \
-                                                       'Sockbot current version: <b>{}</b></i>'.format(pk_id, startup_time, version)
-                                        post_string = html2text(reply_string)
-                                        if subreddit == 'EiteDagerous':
-                                            post_string = 'ಠ_ಠ'
-                                        print('[!] Replying with: {}'.format(post_string))
-                                        comment.reply(post_string)
-                                        pause('Holding after sending message', send_delay)
+                                    print('[-] Comment is already in database')
                 except Exception as e:
                     print('[-] Exception occurred whilst attempting to parse the comments')
                     print('[-] Exception:', e)
