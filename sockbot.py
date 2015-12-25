3c23651796f3d1999ba0813e7992fba8e1378998
Simon AM <simon@agius-muscat.net>
2015-12-25 10:44:02 +0100

    Removed version print, added simple reply

--- sockbot.py
+++ sockbot.py
@@ -11,7 +11,7 @@ for non-commercial purposes. It is not endorsed by nor reflects the views or opi
 no employee of Frontier Developments was involved in the making of it.
 """
 
-testing_mode = False
+testing_mode = True
 
 version = '0.9'
 user_agent = 'raspberrypi:sockb0t259:v{} (by /u/Always_SFW)'.format(version)
@@ -68,7 +68,6 @@ def get_comment_id_list(db, table_name):
 
 
 def main():
-    commenter_timeout = dict()
     cycle = 1
     startup_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     print('[!] Sockbot version {} starting at: {}'.format(version, startup_time))
@@ -89,6 +88,7 @@ def main():
                 print('[+] Sockbot is checking /r/{}  '.format(subreddit))
                 try:
                     for comment in all_comments:
+                        # print('Comment ID: {} Submission: {}'.format(comment.id, comment.submission))
                         print('[+] Checking comment with ID: {}    '.format(comment.id), end='\r')
                         if 'sock' in comment.body.lower() and not comment.author.name == 'sockbot259':
                             avoid_word_present = False  # check for words you are supposed to avoid, like 'Socket'
@@ -118,20 +118,29 @@ def main():
                                     if not subreddit == 'EiteDagerous':
                                         print('[!] Sending string about sock #{}'.format(pk_id))
                                         r.send_message(user, 'Sock #{} spotted!'.format(pk_id), html2text(message_string))  # user, title, contents
-                                    reply_string = '<h1>SOCK DETECTED</h1><br><br>' \
+                                    reply_string = '[ಠ‿ಠ]<br><br>' \
+                                                   '<h1>SOCK DETECTED</h1><br><br>' \
                                                    'tfaddy has been notified.<br><hr><br>' \
                                                    '<i>I am a bot, created and maintained by <a href ="https://www.reddit.com/user/Always_SFW">CMDR Purrcat, /u/Always_SFW</a><br>' \
                                                    'Click <a href="https://www.reddit.com/r/EliteDangerous/comments/3sz817/learn_how_to_get_ripped_in_4_weeks/cx261wx">here</a> to find out why I exist<br>' \
                                                    'You can find my source code <a href="https://github.com/Winter259/sockbot">on github</a><br>' \
                                                    'Socks detected so far: <b>{}</b><br>' \
-                                                   'Online since: <b>{}</b> (GMT)<br>' \
-                                                   'Sockbot current version: <b>{}</b></i><br>'.format(pk_id, startup_time, version)
+                                                   'Online since: <b>{}</b> (GMT)<br>'.format(pk_id, startup_time)
                                     post_string = html2text(reply_string)
                                     post_string += 'Need something to keep your feet warm? How about some [ELITE DANGEROUS SOCKS??](https://www.frontierstore.net/merchandise/elite-dangerous-logo-socks-black.html)'
                                     if subreddit == 'EiteDagerous':
                                         post_string = 'ಠ_ಠ'
+                                    # if the comment is unimaginative...
+                                    if comment.body.lower() == 'sock':
+                                        post_string = 'You could try to be a bit more imaginative with your post...'
                                     print('[!] Replying to comment with ID: {}'.format(comment.id))
                                     comment.reply(post_string)
+                                    """
+                                    if commenter_timeout[comment.author.name] is None:
+                                        commenter_timeout[comment.author.name] = []
+                                    else:
+                                        commenter_timeout[comment.author.name] = commenter_timeout[comment.author.name].append(comment.submission)
+                                    """
                                     pause('Holding after sending message', send_delay)
                                 else:
                                     print('[-] Comment with ID: {} is already in database'.format(comment.id))
